968ac6389bfa92c953da6c59e356edf9851c4db4
Tom.Song <qt.song@appotronics.com>
2015-09-07 19:53:03 +0800

    changed blog

---
+++ javascripts/config.js
@@ -0,0 +1,18 @@
+var disqus_shortname = 'sqitao';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://sysky.org';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'sqitao';						//Your username of GitHub.
+var repos = 'sqitao.github.io';					//Name of repository that contains your blog.
+var sitetitle = 'SYSKY';						//Name of your blog.
+var rss = '';									//Rss address of your blog.
+var suffix = '';								//Suffix of post files, '.' should be included.
+var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
+
+//Sample Config:
+//var disqus_shortname = 'sneezry';
+//var hostbase = 'http://sneezry.com/blog';
+//var githubname = 'sneezry';
+//var repos = 'blog';	
+//var sitetitle = 'Sneezry';
+//var rss = '';
+//var suffix = '.md';
+//var branch = 'gh-pages';
